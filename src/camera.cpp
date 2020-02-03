41b831e1be97ac01f29dfc06a400acb807bfbe4d
Jennifer Lucas <36136983+jclucas@users.noreply.github.com>
2020-02-03 17:49:16 -0500

    improve camera transform

--- src/camera.cpp
+++ src/camera.cpp
@@ -40,26 +40,37 @@ glm::vec3* Camera::render(size_t height, size_t width, Scene scene) {
     glm::vec3 *frame = new glm::vec3[height * width];
 
     // define film plane
-    glm::vec3 center = position + n * length;
+    glm::vec3 center = position + glm::vec3(0, 0, length);
     float aspect = (float) height / width;
     float w = glm::tan(fov) * 2 * length;
     float h = w * aspect;
 
-    // angle to step in world space between pixels
+    // amount to step in camera space between pixels
     float step = w / width;
-
-    // upper left corner ray
-    glm::vec4 ul = glm::vec4(center.x - w / 2 + step / 2, center.y - h / 2 + step / 2, center.z, 1);
+    glm::vec4 dw = glm::vec4(step, 0, 0, 0);
+    glm::vec4 dh = glm::vec4(0, step, 0, 0);
 
     // transform to world space
-    glm::vec3 worldpos = glm::vec4(position.x, position.y, position.z, 1) * glm::inverse(m);
-    ul = ul * glm::inverse(m);
+    glm::mat4 inv = glm::inverse(m);
+    glm::vec3 worldpos = inv * glm::vec4(position, 0);
+    center = inv * glm::vec4(center, 0);
+    dw = inv * dw;
+    dh = inv * dh;
+    
+    // upper left corner ray
+    glm::vec4 ul = glm::vec4(center, 0);
+    ul -= float(width) / 2 * dw;
+    ul -= float(height) / 2 * dh;
+
+    // cout << "ul: " << ul.x << " " << ul.y << " " << ul.z <<endl;
+    // cout << "dw: " << dw.x << " " << dw.y << " " << dw.z <<endl;
+    // cout << "dh: " << dh.x << " " << dh.y << " " << dh.z <<endl;
 
     glm::vec3 dir;
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            dir = glm::normalize(glm::vec3(ul.x + step * j, ul.y + step * i, center.z) - worldpos);
-            frame[i*width + j] = scene.cast(worldpos, dir);
+            dir = glm::normalize(glm::vec3(ul + dw * float(j) + dh * float(i)) - worldpos);
+            frame[i*width + j] = scene.cast(position, dir);
         }
     }
 
